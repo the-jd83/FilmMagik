9a07fb92a9578b53bed3fd76a657583e94af6d28
Jay Dholakiya <113056681+the-jd83@users.noreply.github.com>
2024-04-17 00:34:26 +0530

    firebase-config.js

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -2,14 +2,14 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS6A2yuBxf1LOrNVP6jj3PTkcP_OIUmI0",
-  authDomain: "react-netflix-clone-8ff21.firebaseapp.com",
-  projectId: "react-netflix-clone-8ff21",
-  storageBucket: "react-netflix-clone-8ff21.appspot.com",
-  messagingSenderId: "287858747041",
-  appId: "1:287858747041:web:665aeb4caa658196d7b2e0",
-  measurementId: "G-H3S7F6RQ5Y"
+  apiKey: "AIzaSyC_Uq5JjHD9FwRLVcLTeVKHUQBGUcbvstI",
+  authDomain: "filemagik-518bc.firebaseapp.com",
+  projectId: "filemagik-518bc",
+  storageBucket: "filemagik-518bc.appspot.com",
+  messagingSenderId: "707477494037",
+  appId: "1:707477494037:web:c9857397da6e5012cdf11c"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
